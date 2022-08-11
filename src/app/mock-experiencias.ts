731a3bb5c13cea5049aa6a8e50a0b32735479d20
TomDev <andradestom@gmail.com>
2022-08-11 00:06:25 -0300

    Mock de experiencias e interface

---
+++ src/app/mock-experiencias.ts
@@ -0,0 +1,31 @@
+import { Experiencia } from "./experiencia"
+
+export const EXPERIENCIAS: Experiencia[] = [
+    {
+
+        id: 1,
+        nombreEmpresa: "Fermi Componentes Electronicos",
+        esTrabajoActual: true,
+        fechaInicio: new Date(10 / 3 / 2021),
+        fechaFin: new Date(),
+        descripcion: "Instalador Electrico"
+    },
+    {
+
+        id: 2,
+        nombreEmpresa: "VECO",
+        esTrabajoActual: false,
+        fechaInicio: new Date(1 / 2 / 2022),
+        fechaFin: new Date(10 / 3 / 2021),
+        descripcion: "Operario Tecnico Electromecanico"
+    },
+    {
+
+        id: 3,
+        nombreEmpresa: "Back Srl",
+        esTrabajoActual: true,
+        fechaInicio: new Date(10 / 10 / 2018),
+        fechaFin: new Date(10 / 3 / 2019),
+        descripcion: "Operario"
+    }
+]
