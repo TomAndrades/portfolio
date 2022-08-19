1d4b0a2e2e3054218be22e75eb34ac37a82dc58b
TomDev <andradestom@gmail.com>
2022-08-18 21:10:39 -0300

    Creacion y vinculacion a mock de experiencias

--- src/app/mock-experiencias.ts
+++ src/app/mock-experiencias.ts
@@ -6,7 +6,7 @@ export const EXPERIENCIAS: Experiencia[] = [
         id: 1,
         nombreEmpresa: "Fermi Componentes Electronicos",
         esTrabajoActual: true,
-        fechaInicio: new Date(10 / 3 / 2021),
+        fechaInicio: new Date("10/03/2021"),
         descripcion: "Instalador Electrico"
     },
     {
@@ -14,17 +14,17 @@ export const EXPERIENCIAS: Experiencia[] = [
         id: 2,
         nombreEmpresa: "VECO",
         esTrabajoActual: false,
-        fechaInicio: new Date(1 / 2 / 2022),
-        fechaFin: new Date(10 / 3 / 2021),
+        fechaInicio: new Date("01/02/2022"),
+        fechaFin: new Date("10/03/2021"),
         descripcion: "Operario Tecnico Electromecanico"
     },
     {
 
         id: 3,
         nombreEmpresa: "Back Srl",
-        esTrabajoActual: true,
-        fechaInicio: new Date(10 / 10 / 2018),
-        fechaFin: new Date(10 / 3 / 2019),
+        esTrabajoActual: false,
+        fechaInicio: new Date("10/10/2018"),
+        fechaFin: new Date("10/03/2019"),
         descripcion: "Operario"
     }
 ]
